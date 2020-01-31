476a73dd3b6d92d5a646838d0670239f2a5e19b9
Debashish Paul <debashish333paul@gmail.com>
2020-01-31 11:48:35 +0600

    route of react 3 aproach

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <h3>logo</h3>
+        <ul className="allign">
+          <li>home</li>
+          <li>about</li>
+          <li>shop</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
